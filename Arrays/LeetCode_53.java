66fbf36a38d4309ae4ebbfa4de159b0b08522743
tanujbansal06 <tanujbansal0051@gmail.com>
2026-03-22 11:55:50 +0530

    Add files via upload

---
+++ Arrays/LeetCode_53.java
@@ -0,0 +1,13 @@
+class LeetCode_53 {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
