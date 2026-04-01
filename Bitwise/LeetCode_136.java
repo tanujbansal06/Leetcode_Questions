21af8f1ba713a5f2ea937c166b4cd3ce3c4a2a56
tanujbansal06 <tanujbansal0051@gmail.com>
2026-04-01 08:42:45 +0530

    Update LeetCode_136.java

--- Bitwise/LeetCode_136.java
+++ Bitwise/LeetCode_136.java
@@ -1,5 +1,10 @@
-class Solution {
-    public int singleNumber(int[] nums) {
+public class LeetCode_136 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,3,4,2,2,3};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
         int xor=0;
         for(int i =0;i<nums.length;i++){
             xor = xor ^ nums[i];
@@ -7,4 +12,6 @@ class Solution {
         return xor;
     }
 }
-//a^a = 0 and a^0 = a --> means all the paired elements will get 0 and remaining single element having xor with 0 will give itself as answer
+
+//a^a = 0 and a^0 = a --> means all the paired elements will get 0 and remaining single element 
+//having xor with 0 will give itself as answer
