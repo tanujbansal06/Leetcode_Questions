e3f86002de7f758cd6a9e3b1e409576d91e157a3
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-08-13 22:23:35 +0530

    PostOrder traversal

---
+++ Tree/LeetCode_145.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class LeetCode_145 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            // left right root
+            List<Integer> ans = new ArrayList<>();
+            helper(root, ans);
+            return ans;
+        }
+
+        void helper(TreeNode node, List<Integer> ans) {
+            if (node == null)
+                return;
+            helper(node.left, ans);
+            helper(node.right, ans);
+            ans.add(node.val);
+        }
+    }
+}
