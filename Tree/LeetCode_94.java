5c689e0dadb91ee2da0f0f69a1b200593d93c8d7
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-08-13 11:12:51 +0530

    Inorder traversal

---
+++ Tree/LeetCode_94.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class LeetCode_94 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            inOrder(root, ans);
+            return ans;
+        }
+
+        void inOrder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left, ans);
+            ans.add(root.val);
+            inOrder(root.right, ans);
+        }
+    }
+}
