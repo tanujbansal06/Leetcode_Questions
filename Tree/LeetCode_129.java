70e7388cdcca8b1c3d9060915443b7002c446e1d
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-08-13 11:24:27 +0530

    Sum of root to leaf node

---
+++ Tree/LeetCode_129.java
@@ -0,0 +1,31 @@
+public class LeetCode_129 {
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return helper(root,0);
+    }
+
+    int helper(TreeNode root, int sum){
+        if(root == null) return 0;
+        sum = sum*10+root.val;
+        if(root.left == null && root.right == null){
+            return sum;
+        }
+        int left = helper(root.left,sum);
+        int right = helper(root.right,sum);
+        return left+right;
+    }
+}
+}
