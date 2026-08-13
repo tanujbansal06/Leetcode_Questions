566ca8e7971bf6fed79c2667dc7d9de4bfc4cc78
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-08-13 12:21:50 +0530

    Path sum

---
+++ Tree/LeetCode_112.java
@@ -0,0 +1,27 @@
+public class LeetCode_112 {
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        targetSum -= root.val;
+        if(root.left == null && root.right == null){
+            return targetSum == 0;
+        }
+        boolean left = hasPathSum(root.left, targetSum);
+        boolean right = hasPathSum(root.right, targetSum);
+        return left || right;
+    }
+}
+}
