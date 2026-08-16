3c75b594b2f2dc3b2af48fc07d88de6fa1d0ab0c
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-08-16 18:53:02 +0530

    Level order traversal 2

---
+++ Tree/LeetCode_107.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class LeetCode_107 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Queue<TreeNode> queue = new ArrayDeque<>();
+            if (root == null)
+                return ans;
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> lst = new ArrayList<>();
+                int n = queue.size();
+                for (int i = 0; i < n; i++) {
+                    TreeNode node = queue.poll();
+                    lst.add(node.val);
+                    if (node.left != null)
+                        queue.offer(node.left);
+                    if (node.right != null)
+                        queue.offer(node.right);
+                }
+                ans.add(lst);
+            }
+            Collections.reverse(ans);
+            return ans;
+        }
+    }
+}
