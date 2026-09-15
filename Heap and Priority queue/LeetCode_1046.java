df9760729eaa51d33a329175cb1b76eb9086e69a
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-09-15 21:52:51 +0530

    last stone

---
+++ Heap and Priority queue/LeetCode_1046.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class LeetCode_1046 {
+    class Solution {
+        public int lastStoneWeight(int[] stones) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            for (int stone : stones) {
+                pq.add(stone);
+            }
+            while (pq.size() > 1) {
+                int y = pq.poll();
+                int x = pq.poll();
+                if (x != y) {
+                    pq.add(y - x);
+                }
+            }
+            return pq.isEmpty() ? 0 : pq.peek();
+        }
+    }
+}
