af13ebdacf806b0409bb9ed269572a66813b480e
Tanuj Bansal <tanujbansal0051@gmail.com>
2026-09-16 11:40:39 +0530

    Intersection

--- Map and set/LeetCode_349.java
+++ Map and set/LeetCode_349.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class LeetCode_349 {
     public static void main(String[] args) {
         int[] nums1 = { 1, 2, 2, 1 };
@@ -6,28 +7,26 @@ public class LeetCode_349 {
         System.out.println(intersection(nums1, nums2));
     }
 
-    public static  int[] intersection(int[] nums1, int[] nums2) {
+    public static int[] intersection(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums1.length; i++) {
-            if (map.containsKey(nums1[i])) {
-                map.put(nums1[i], map.get(nums1[i]) + 1);
-            } else {
-                map.put(nums1[i], 1);
-            }
+            map.put(nums1[i], 1);
         }
-
         List<Integer> ll = new ArrayList<>();
-        for(int i =0;i<nums2.length;i++){
-            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
                 ll.add(nums2[i]);
-                map.put(nums2[i], map.get(nums2[i])-1);
+                map.put(nums2[i], 0);
             }
         }
 
         int[] ans = new int[ll.size()];
-        for(int i =0;i<ans.length;i++){
+
+        for (int i = 0; i < ans.length; i++) {
             ans[i] = ll.get(i);
         }
+
         return ans;
     }
 }
